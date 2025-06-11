916998c15fd1ec64ee86e5e5e8a3317896b5d640
Eduardo Schoof <138504901+DevSchoof@users.noreply.github.com>
2025-06-11 00:05:55 +0000

    corrigindo vercel

--- backend/run.py
+++ backend/run.py
@@ -4,3 +4,7 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+else:
+    # Isso é necessário para o gunicorn/vercel encontrar o app
+    application = app  # Renomeia para 'application' (padrão WSGI)
